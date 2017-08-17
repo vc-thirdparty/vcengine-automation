4c003dad26e350dccf67f3678e1457fbdbe2c240
Erik Ramfelt <erik.ramfelt@flexlink.com>
2017-08-17 11:50:31 +0200

    Added video and experience actions

---
+++ src/VcEngineAutomation/Actions/RecordExperienceAction.cs
@@ -0,0 +1,57 @@
+﻿using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Definitions;
+using System;
+using System.Linq;
+using VcEngineAutomation.Panels;
+using VcEngineAutomation.Windows;
+
+namespace VcEngineAutomation.Actions
+{
+    public class RecordExperienceAction : IDisposable
+    {
+        private readonly VcEngine vcEngine;
+        private CommandPanel recordPanel;
+
+        public RecordExperienceAction(VcEngine vcEngine)
+        {
+            this.vcEngine = vcEngine;
+        }
+
+        public void RecordFor(string filename, TimeSpan duration)
+        {
+            StartRecording(filename);
+            var simulationPanel = SimulationPanel.Attach(vcEngine);
+            simulationPanel.WaitFor(simulationPanel.ElapsedSimulationTime + duration);
+            StopRecording();
+        }
+
+        public void StartRecording(string filename)
+        {
+            var simulationPanel = SimulationPanel.Attach(vcEngine);
+            simulationPanel.Panel.FindFirstDescendant("vcaxRecordingButton").AsButton().Invoke();
+            recordPanel = vcEngine.GetCommandPanel();
+            Button[] buttons = recordPanel.CustomPane.FindAllDescendants(cf => cf.ByControlType(ControlType.Button)).Select(ae => ae.AsButton()).ToArray();
+            buttons[0].Invoke();
+            FileDialog.Attach(vcEngine).Save(filename, waitForWriteIsCompleted: false);
+        }
+
+        public void StopRecording()
+        {
+            if (recordPanel != null)
+            {
+                Button[] buttons = recordPanel.CustomPane.FindAllDescendants(cf => cf.ByControlType(ControlType.Button)).Select(ae => ae.AsButton()).ToArray();
+                buttons[1].Invoke();
+                recordPanel.Close();
+            }
+            recordPanel = null;
+        }
+
+        public void Dispose()
+        {
+            if (recordPanel != null)
+            {
+                StopRecording();
+            }
+        }
+    }
+}
---
+++ src/VcEngineAutomation/Actions/RecordVideoAction.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Definitions;
+using VcEngineAutomation.Panels;
+using VcEngineAutomation.Windows;
+
+namespace VcEngineAutomation.Actions
+{
+    public class RecordVideoAction : IDisposable
+    {
+        private readonly VcEngine vcEngine;
+        private CommandPanel recordPanel;
+
+        public RecordVideoAction(VcEngine vcEngine)
+        {
+            this.vcEngine = vcEngine;
+        }
+
+        public void RecordFor(string filename, TimeSpan duration)
+        {
+            StartRecording(filename);
+            var simulationPanel = SimulationPanel.Attach(vcEngine);
+            simulationPanel.WaitFor(simulationPanel.ElapsedSimulationTime + duration);
+            StopRecording();
+        }
+
+        public void StartRecording(string filename)
+        {
+            var simulationPanel = SimulationPanel.Attach(vcEngine);
+            var videoButton = simulationPanel.Panel.FindFirstDescendant("VideoRecordingButton");
+            if (videoButton == null) throw new InvalidOperationException("Video recording is not activated. Please activate it in the exe.config.");
+            videoButton.AsButton().Invoke();
+            recordPanel = vcEngine.GetCommandPanel();
+            Button[] buttons = recordPanel.CustomPane.FindAllDescendants(cf => cf.ByControlType(ControlType.Button)).Select(ae => ae.AsButton()).ToArray();
+            buttons[0].Invoke();
+            FileDialog.Attach(vcEngine).Save(filename, waitForWriteIsCompleted: false);
+        }
+
+        public void StopRecording()
+        {
+            if (recordPanel != null)
+            {
+                Button[] buttons = recordPanel.CustomPane.FindAllDescendants(cf => cf.ByControlType(ControlType.Button)).Select(ae => ae.AsButton()).ToArray();
+                buttons[1].Invoke();
+                recordPanel.Close();
+            }
+            recordPanel = null;
+        }
+
+        public void Dispose()
+        {
+            if (recordPanel != null)
+            {
+                StopRecording();
+            }
+        }
+    }
+}
